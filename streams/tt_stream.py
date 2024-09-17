aeac4a594ba4f2b12ad8e36643f0bbcd663c9363
herrmaja <jona.herrmann@ipvs.uni-stuttgart.de>
2024-09-17 14:10:50 +0200

    Merge manually omnet branch into main

--- streams/tt_stream.py
+++ streams/tt_stream.py
@@ -1,5 +1,13 @@
 from dataclasses import dataclass
+import json
+from typing import Dict
 
+try:
+    import graph_tool.all as gt
+
+    hasGt = True
+except:
+    hasGt = False
 
 @dataclass
 class TtStream:
@@ -10,6 +18,7 @@ class TtStream:
     frame_size_byte: int
     cycle_time_ns: int
     deadline_ns: int
+    properties: dict
     et_capable: bool = False
 
     def __init__(self, stream_id: int):
@@ -35,4 +44,27 @@ def from_json(json_object) -> TtStream:
     stream.cycle_time_ns = json_object['cycle_time_ns']
     stream.deadline_ns = json_object['deadline_ns']
     stream.et_capable = json_object['et_capable']
+    stream.properties = {}
     return stream
+
+def parse_streams(topology, stream_path: str) -> Dict[int, TtStream]:
+    streams = {}
+
+    with open(stream_path) as stream_fd:
+        stream_data = json.load(stream_fd)
+
+        for stream_json in stream_data:
+            stream = TtStream(stream_json['id'])
+            stream.name = stream_json['name']
+            stream.source = stream_json['source']
+            stream.target = stream_json['target']
+            stream.frame_size_byte = stream_json['frame_size_byte']
+            stream.cycle_time_ns = stream_json['cycle_time_ns']
+            stream.deadline_ns = stream_json['deadline_ns']
+            stream.properties = {}
+            if hasGt:
+                stream.properties["source"] = topology.vertex(stream.source)
+                stream.properties["target"] = topology.vertex(stream.target)
+            streams[stream.id] = stream
+
+    return streams
