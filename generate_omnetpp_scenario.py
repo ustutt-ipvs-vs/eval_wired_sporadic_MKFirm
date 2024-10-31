75954ed9e7ce462637adb0b932b277fa92eb94f5
Lucas Haug <lucas.haug@ipvs.uni-stuttgart.de>
2024-10-31 15:24:14 +0100

    OMNeT++ Generation probably working

--- generate_omnetpp_scenario.py
+++ generate_omnetpp_scenario.py
@@ -1,6 +1,6 @@
 import argparse
 import json
-import os
+import os.path
 from typing import Dict, List
 
 from streams.tt_stream import parse_streams, TtStream
@@ -50,7 +50,7 @@ connection_base = '''
         {src}.ethg[{src_port}] <--> EthernetLink {{ length = {length}m; datarate = {datarate}Mbps; }} <--> {tgt}.ethg[{tgt_port}];'''
 
 
-def generate_network(topology):
+def generate_network(topology, output):
     devices = {}
 
     for vertex in topology.vertices():
@@ -133,7 +133,7 @@ def generate_network(topology):
             devices[connection["tgt_id"]]["connections"][connection["src_id"]]["done"] = True
 
     # Write to file
-    with open("Scenario.ned", "w") as f:
+    with open(os.path.join(output, "Scenario.ned"), "w") as f:
         f.write(network_base.format(submodules=submodules, connections=connections))
     return devices
 
@@ -173,7 +173,7 @@ omnet_device_base = '''
 
 mac_config_base = '''{{"address": "{address}", "interface": "eth{port}"}}'''
 
-src_app_base = '''
+src_app_base_tt = '''
 *.{device}.app[{index}].typename = "UdpSourceApp"
 *.{device}.app[{index}].display-name = "{name}"
 *.{device}.app[{index}].io.destAddress = "{ip}" # {tgt}
@@ -181,6 +181,20 @@ src_app_base = '''
 *.{device}.app[{index}].source.packetLength = {size}B - 58B # 58B = 4B (VLAN Tag) + 8B (UDP) + 20B (IP) + 14B (ETH MAC) + 4B (ETH FCS) + 8B (ETH PHY)
 *.{device}.app[{index}].source.productionInterval = {prod_interval}
 *.{device}.app[{index}].source.initialProductionOffset = {offset}ns
+*.{device}.app[{index}].io.interfaceTableModule = "^.^.interfaceTable"
+'''
+
+src_app_base_et = '''
+*.{device}.app[{index}].typename = "UdpBasicBurst"
+*.{device}.app[{index}].display-name = "{name}"
+*.{device}.app[{index}].destAddresses = "{ip}" # {tgt}
+*.{device}.app[{index}].destPort = {port}
+*.{device}.app[{index}].chooseDestAddrMode = "once"
+*.{device}.app[{index}].burstDuration = {burst_duration}ms
+*.{device}.app[{index}].messageLength = {size}B - 58B # 58B = 4B (VLAN Tag) + 8B (UDP) + 20B (IP) + 14B (ETH MAC) + 4B (ETH FCS) + 8B (ETH PHY)
+*.{device}.app[{index}].startTime = {interevent_time}ms + exponential({exp_param}ms)
+*.{device}.app[{index}].sleepDuration = {interevent_time}ms + exponential({exp_param}ms)
+*.{device}.app[{index}].sendInterval = 9999s # burst consists of a single packet
 *.{device}.app[{index}].io.interfaceTableModule = "^.^.interfaceTable"
 '''
 
@@ -212,7 +226,9 @@ def add_mac_entries(devices, stream_properties):
         })
 
 
-def generate_omnetpp_ini(topology, streams: Dict[int, TtStream], emergency_streams, devices, gcls):
+
+
+def generate_omnetpp_ini(topology, streams: Dict[int, TtStream], emergency_streams, devices, gcls, output):
     identifier_mapping = []
     pcp_mappings = []
     generate_apps(devices, identifier_mapping, pcp_mappings, streams, topology)
@@ -223,7 +239,9 @@ def generate_omnetpp_ini(topology, streams: Dict[int, TtStream], emergency_strea
         app_str = ""
         for app in device["apps"]:
             if app["type"] == "UdpSourceApp":
-                app_str += src_app_base.format(**app)
+                app_str += src_app_base_tt.format(**app)
+            elif app["type"] == "UdpBasicBurst":
+                app_str += src_app_base_et.format(**app)
             else:
                 app_str += sink_app_base.format(**app)
 
@@ -241,7 +259,7 @@ def generate_omnetpp_ini(topology, streams: Dict[int, TtStream], emergency_strea
                 gcl_entry["durations"] = ", ".join(str(d) + "ns" for d in gcl_entry["durations"])
                 gcl_str += gcl_base.format(name=devices[device_id]["name"], port=port, pcp=pcp, **gcl_entry)
 
-    with open("omnetpp.ini", "w") as f:
+    with open(os.path.join(output, "omnetpp.ini"), "w") as f:
         f.write(ini_base.format(apps=device_str,
                                 identifier_mapping=identifier_mapping_spacing.join(identifier_mapping),
                                 pcp_mapping=pcp_mapping_spacing.join(pcp_mappings), gcls=gcl_str))
@@ -285,81 +303,27 @@ def generate_apps(devices, identifier_mapping, pcp_mappings, streams, topology):
         port += 1
 
 
-# Sieve of Eratosthenes
-# Code by David Eppstein, UC Irvine, 28 Feb 2002
-# http://code.activestate.com/recipes/117119/
-def gen_primes():
-    """ Generate an infinite sequence of prime numbers.
-    """
-    # Maps composites to primes witnessing their compositeness.
-    # This is memory efficient, as the sieve is not "run forward"
-    # indefinitely, but only as long as required by the current
-    # number being tested.
-    #
-    D = {}
-
-    # The running integer that's checked for primeness
-    q = 2
-
-    while True:
-        if q not in D:
-            # q is a new prime.
-            # Yield it and mark its first multiple that isn't
-            # already marked in previous iterations
-            #
-            yield q
-            D[q * q] = [q]
-        else:
-            # q is composite. D[q] is the list of primes that
-            # divide it. Since we've reached q, we no longer
-            # need it in the map, but we'll mark the next
-            # multiples of its witnesses to prepare for larger
-            # numbers
-            #
-            for p in D[q]:
-                D.setdefault(p + q, []).append(p)
-            del D[q]
-
-        q += 1
-
-
-emergeny_stream_interval_base = '''replaceUnit(1 / (0.125 * (sin(dropUnit(simTime() * 1)) + sin(dropUnit(simTime() * {prime}))) + {shift}), "ms")'''
-def calc_production_intervals(emergency_streams):
-    # TODO: Use last prime to calculate simtime
-    prime_generator = gen_primes()
-    for stream in emergency_streams:
-        # We want all possible overlaps of the emergency streams, that's why we use prime numbers to generate the frequency
-        prime = next(prime_generator)
-        # The median of the sin function is 0, so we can shift it up depending on frame size to get the desired bitrate:
-        bitrate = stream["rate_mbps"]
-        # Do not subtract the -58B overhead, because we want to calculate the datarate on wire not on the payload
-        frame_size = stream["frame_size_byte"] # +58B = 4B (VLAN Tag) + 8B (UDP) + 20B (IP) + 14B (ETH MAC) + 4B (ETH FCS) + 8B (ETH PHY)
-
-        frames_per_ms = bitrate * 1e3 / (frame_size * 8)
-
-        if frames_per_ms < 0.25:
-            raise ValueError("Bitrate too low for frame size, we can fix this by reducing the amplitude of the sin curve")
-
-        stream["production_interval"] = emergeny_stream_interval_base.format(prime=prime, shift=frames_per_ms)
-
 
 def generate_emergency_apps(devices, identifier_mapping, pcp_mappings, emergency_streams, topology):
     port = 10000
-    calc_production_intervals(emergency_streams)
     for stream in emergency_streams:
         device = devices[topology.vp.v_id[stream["source"]]]
         tgt_device = devices[topology.vp.v_id[stream["target"]]]
+        drate = device["datarates"][stream["properties"]["route"][0]["port"]]
+        burst_duration = stream["bucket_size_byte"] / (drate - stream["rate_mbps"]) * 8 / 1e3
+
         src_app = {
-            "type": "UdpSourceApp",
+            "type": "UdpBasicBurst",
             "device": device["name"],
             "tgt": tgt_device["name"],
             "ip": stream["properties"]["ip"],
             "name": stream["name"],
             "port": port,
             "size": stream["frame_size_byte"],
-            "prod_interval": stream["production_interval"],
             "index": len(device["apps"]),
-            "offset": 0,
+            "exp_param": 10,
+            "burst_duration": burst_duration,
+            "interevent_time": stream["min_inter_event_time_ns"] /1e6,
         }
         devices[topology.vp.v_id[stream["source"]]]["apps"].append(src_app)
 
@@ -407,6 +371,7 @@ def add_route_to_emergency_streams(e_streams, devices):
                 "port": devices[src_str]["connections"][str(route_element["to"])]["src_port"]
             })
     pass
+
 
 
 def load_gcls(gcl_path, streams, devices):
@@ -528,6 +493,7 @@ def parse_emergency_streams(path):
         return json.load(emergency_stream_fd)
 
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--topology', '-t', help="Topology path", type=str, required=True)
@@ -535,17 +501,18 @@ if __name__ == "__main__":
     parser.add_argument('--emergency_streams', '-e', help="Emergency stream path", type=str, required=True)
     parser.add_argument('--transmission', '-m', help="Transmission path", type=str, required=False)
     parser.add_argument("--gcl", "-g", help="Path to GCL output", type=str, required=False)
+    parser.add_argument("--output", "-o", help="Output path", type=str, required=False)
     args = parser.parse_args()
 
     topology = parse_topology(args.topology)
     streams = parse_streams(topology, args.streams)
     e_streams = parse_emergency_streams(args.emergency_streams)
     extend_streams_with_multicast(streams, e_streams)
-    devices = generate_network(topology)
+    devices = generate_network(topology, args.output)
 
     gcls = load_gcls(args.gcl, streams, devices)
     parse_transmission_output(args.transmission, devices, streams)
     add_route_to_emergency_streams(e_streams, devices)
-    generate_omnetpp_ini(topology, streams, e_streams, devices, gcls)
+    generate_omnetpp_ini(topology, streams, e_streams, devices, gcls, args.output)
 
     print(topology, streams)
